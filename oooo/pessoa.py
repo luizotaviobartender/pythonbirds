f8f439476e767ead5f6592063155b6f8885ca07c
otavio <luizotaviobartender@gmail.com>
2020-03-28 09:29:48 -0300

    metodo criado

--- oooo/pessoa.py
+++ oooo/pessoa.py
@@ -1,12 +1,12 @@
 class Pessoa:
-    def __init__(self):
-        self.name = None
+    def __init__(self,nome=None):
+        self.name = nome
 
     def cumprimenta(self):
         return f'olá {id(self)}'
 
 if __name__=='__main__':
-    p = Pessoa()
+    p = Pessoa('luiz')
     print(Pessoa.cumprimenta(p))
     print(id(p))
     print(p.cumprimenta())

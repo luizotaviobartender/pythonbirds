0bcf9c05fa67515fea22a93603b0b7547a6319e8
otavio <luizotaviobartender@gmail.com>
2020-03-29 09:36:47 -0300

    interação com o usuario

---
+++ innovetion curso py.py
@@ -0,0 +1,11 @@
+a =int( input( 'entre com o primeiro valor' ))
+b =int( input( "segundo tambem" ))
+soma = (a + b)
+subtraçao = (a - b)
+multiplicaçao =(a * b)
+print('Soma: {soma}. \nSubtraçao: {sub}.\nmultiplicaçao: {mult}'.format(soma=soma,sub=subtraçao,mult=multiplicaçao))
+
+#x = ('1')
+#soma2 = int('x') + 1
+# print(soma2)
+
